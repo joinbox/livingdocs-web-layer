4ffc6b9aa1ff751ae464d1053504bc42fb7ae070
mruefenacht <ruefe@rcky.ch>
2016-11-21 12:15:02 +0100

    Removed the app middleware since request.app seems to be available

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ nunjucks.configure('www/templates', {
     , watch     : true
 });
 
-app.use('*', (req, resp, next)=>{
-    if(!('appData' in req)) req.appData = {};
-    req.appData.rootApp = app;
-    next();
-});
-
 app.use('/', require('./routes'));
 app.listen(appConfig.port || defaultPort, () => {
    console.log('App listening on port ' + (appConfig.port || defaultPort));
